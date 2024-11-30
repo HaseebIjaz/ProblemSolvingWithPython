2c444f92c8239363a9bfff8d5655e2fac087a214
Haseeb Ijaz <haseebijazi150172@gmail.com>
2024-12-01 00:02:24 +0500

    Added two more solutions

--- twoSum/solution.py
+++ twoSum/solution.py
@@ -5,3 +5,26 @@ class Solution:
             for j in range(i+1, len(nums)):
                 if nums[i] + nums[j] == target:
                     return [i,j]
+
+class Solution1:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hashmap = {}
+        for i in range(len(nums)):
+            hashmap[nums[i]] = i
+        for i in range(i+1, len(nums)):
+            complement: int = target - nums[i]
+            if complement in hashmap and hashmap[complement] != i :
+                return [i, hashmap[complement]]
+        # if a pair is not found return empty array
+        return []
+    
+class Solution2:
+    def twoSum(self, nums:List[int], target:int) ->List[int] :
+        hashtable = {}
+        for i in range(len(nums)):
+            complement = target - nums[i]
+            if complement in hashtable:
+                return [i, hashtable[complement]]
+            else:
+                hashtable[complement] = i
+        return []
