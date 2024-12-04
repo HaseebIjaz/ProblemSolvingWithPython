637b836b8e3ac10fcff864aca817fa5e52fda345
Haseeb Ijaz <haseebijazi150172@gmail.com>
2024-12-04 23:58:08 +0500

    solution practice

--- twoSumIV/solution.py
+++ twoSumIV/solution.py
@@ -60,4 +60,20 @@ class Solution:
         return False
 
             
-        
+class Solution:
+    def findTarget(self, root: Optional[TreeNode], k:int) -> bool:
+        if root is None:
+            return False
+        nodes_to_visit = [root]
+        visited_nodes_hash_set = set()
+        while nodes_to_visit:
+            current_node = nodes_to_visit.pop(0)
+            complement = k - current_node.val
+            if complement in visited_nodes_hash_set:
+                return True
+            visited_nodes_hash_set.add(current_node.val)
+            if current_node.left:
+                nodes_to_visit.append(current_node.left)
+            if current_node.right:
+                nodes_to_visit.append(current_node.right)
+        return False
