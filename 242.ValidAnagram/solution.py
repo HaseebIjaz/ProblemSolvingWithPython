6b7ddc2d2f81c2675162443da29b3e9783cb8935
Haseeb Ijaz <haseebijazi150172@gmail.com>
2025-01-29 12:31:44 +0500

    Valid Anagram  and Sorting Solution

---
+++ 242.ValidAnagram/solution.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        # After this line lenghts are equal
+
+## Sorting only makes sense for mutable data structures like list
+## It does not make sense for immutable data structires like string
+        s.sort()
+        t.sort()
+        for i in range(len(s)):
+            if s[i] != t[i]:
+                return False
+        return True
