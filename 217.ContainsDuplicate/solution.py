ccd07fb529595f573419069b5e169c2c20f208c3
Haseeb Ijaz <haseebijazi150172@gmail.com>
2025-01-29 12:18:28 +0500

    Other solutions for duplicates check

--- 217.ContainsDuplicate/solution.py
+++ 217.ContainsDuplicate/solution.py
@@ -1,4 +1,5 @@
-# Solution with Hashset Space Complexity : O(1) Time Complexity : O(N) 
+# Solution with Hashset 
+# Space Complexity : O(N) Time Complexity : O(N) 
 class Solution:
     def containsDuplicate(self, nums:List[int]) -> bool :
         hashset = set()
@@ -7,4 +8,34 @@ class Solution:
             if num in hashset:
                 return True
             hashset.add(num)
+        return False
+
+# Solution with Hashset Length
+# Space Complexity : O(N) Time Complexity : O(N) 
+
+class Solution:
+    def containsDuplicate(self, nums:List[int]) -> bool:
+        return len(set(nums)) < len(nums) 
+
+
+## Solution with Sorted Array
+## Time Complexity O(NlogN)
+## Space Complexity O(1) or O(n) depending on sorting algo
+class Solution:
+    def containsDuplicate(self, nums:List[int]) -> bool:
+        nums.sort()
+        for i in range(1,len(nums)):
+            if nums[i] == nums[i-1]:
+                return True
+        return False
+    
+## Brute Force
+## Space Complexity O(n squared)
+## Time Complexity O(1)
+class Solution:
+    def containsDuplicate(self, nums:List[int]) -> bool:
+        for i in range(len(nums)):
+            for j in range(i+1, len(nums)):
+                if nums[i] == nums[j]:
+                    return True
         return False
