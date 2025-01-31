d985f30c288857008ac28c204559312aaf26ccc0
Haseeb Ijaz <haseebijazi150172@gmail.com>
2025-01-31 11:04:59 +0500

    Guard Clause Comment

--- 242.ValidAnagram/solution.py
+++ 242.ValidAnagram/solution.py
@@ -1,7 +1,9 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
+        # If Lengths are not equal return early, dont sort.
         if len(s) != len(t):
             return False
+        
         return sorted(s) == sorted(t)
     
 
