8a13e82d2bedee5c648d597c1e77fe3d6cd80c69
Haseeb Ijaz <haseebijazi150172@gmail.com>
2025-02-07 07:12:14 +0500

    product expect self

--- 238.ProductofArrayExceptSelf/solution.py
+++ 238.ProductofArrayExceptSelf/solution.py
@@ -12,4 +12,21 @@ class Solution:
             res[i] = product
         
         return res
-    
+
+class Solution:
+    def productExceptSelf(self, nums:List[int]) -> List[int]:
+        n = len(nums)
+        res = [1] * n
+
+        prefix = 1
+        for i in range(n):
+            res[i] = prefix 
+            prefix *= nums[i]
+        
+        postfix = 1
+        for i in range(n-1,-1,-1):
+            res[i] *= postfix
+            postfix *= nums[i]
+
+        return res 
+
