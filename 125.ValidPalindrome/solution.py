d8677ae1c6b2fb8bba0837e19ad8a441f01367f1
Haseeb Ijaz <haseebijazi150172@gmail.com>
2025-02-10 15:31:13 +0500

    Valid Palindrome with .isalnum() and [::-1] to reverse a string

--- 125.ValidPalindrome/solution.py
+++ 125.ValidPalindrome/solution.py
@@ -1,10 +1,21 @@
+# class Solution:
+#     def isPalindrome(self, s: str) -> bool:
+#         s = "".join(s.split(" "))
+#         n = len(s)
+#         for i in range(n//2):
+#             print(i)
+#             print(n)
+#             if s[i] != s[n-1-i]:
+#                 return False
+#         return True
+
 class Solution:
-    def isPalindrome(self, s: str) -> bool:
-        s = "".join(s.split(" "))
-        n = len(s)
-        for i in range(n//2):
-            print(i)
-            print(n)
-            if s[i] != s[n-1-i]:
-                return False
-        return True
+    def isPalindrome(self, s:str) -> bool:
+        if len(s) <= 1: return True
+        
+        new_s = ''
+        for c in s:
+            if c.isalnum():
+                new_s += c.lower()
+
+        return new_s == new_s[::-1]
