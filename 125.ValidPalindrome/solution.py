96672d73e3362677836e04ca6919f81ec6cd567e
Haseeb Ijaz <haseebijazi150172@gmail.com>
2025-02-10 16:18:15 +0500

    l, r left and right pointer

--- 125.ValidPalindrome/solution.py
+++ 125.ValidPalindrome/solution.py
@@ -12,10 +12,28 @@
 class Solution:
     def isPalindrome(self, s:str) -> bool:
         if len(s) <= 1: return True
-        
+
         new_s = ''
         for c in s:
             if c.isalnum():
                 new_s += c.lower()
 
         return new_s == new_s[::-1]
+
+class Solution:
+    def isAlphaNum(self, c:str) -> bool:
+        return (ord('A') <= ord(c) and ord(c) <= ord('Z') or
+                ord('a') <= ord(c) and ord(c) <= ord('z') or
+                ord('0') <= ord(c) and ord(c) <= ord('9'))
+
+    def isPalindrome(self, s:str) -> bool:
+        l, r = 0, len(s) - 1
+
+        while l < r:
+            while l < r and not self.isAlphaNum(s[l]):
+                l += 1
+            while l < r and not self.isAlphaNum(s[r]):
+                r -= 1
+            if s[l] != s[r]:
+                return False
+            l, r = l + 1, r - 1 
