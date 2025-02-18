0403c2414165bf34ba5f6f1c205712739e750ce2
Haseeb Ijaz <haseebijazi150172@gmail.com>
2025-02-18 12:31:43 +0500

    maxArea via 2 pointers

--- 11. Container With Most Water/solution.py
+++ 11. Container With Most Water/solution.py
@@ -11,3 +11,20 @@ class Solution:
                 if area > max_area :
                     max_area = area
         return max_area
+    
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        n = len(height)
+        if n == 2: return min(height[0],height[1])
+        max_area = 0
+        l, r = 0, n-1
+        while l< r:
+            h = min(height[l],height[r])
+            w = r - l
+            area = h * w
+            max_area = max(area, max_area)
+            if height[l] <= height[r]:
+                l += 1
+            else:
+                r -= 1
+        return max_area
