15ccc64cabb2942f11b94e0e62dbbfc9a12c36e8
Haseeb Ijaz <haseebijazi150172@gmail.com>
2025-03-12 02:41:42 +0500

    max profit

---
+++ 121. Best Time to Buy and Sell Stock/solution.py
@@ -0,0 +1,15 @@
+class Solution:
+    def maxProfit(self, prices:List[int]) -> int:
+        n = len(prices)
+        if not prices : return 0
+        maxProfit = 0
+        minBuy = prices[0]
+
+        for i in range(1, n):
+            sell = prices[i]
+            profit = sell - minBuy
+            maxProfit = max(maxProfit, profit)
+            minBuy = min(minBuy, prices[i])
+        
+        return maxProfit
+        #MaxProfit
